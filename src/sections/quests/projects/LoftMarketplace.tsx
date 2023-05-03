bae807ce4e628416d27618eb820aeceb06a599ee
Leonardo Giroto <leonardo_giroto@hotmail.com>
2023-05-03 19:44:43 -0300

    feat: updates on loft marketplace text

--- src/sections/quests/projects/LoftMarketplace.tsx
+++ src/sections/quests/projects/LoftMarketplace.tsx
@@ -4,7 +4,11 @@ export const LoftMarketplaceQuest = () => {
       Real State Marketplace.<br /><br />
       Product team taking care of the most strategic products for the company, such as the home details page, creating scalable and engaging experiences. Had big challenges on a lot of legacy code to be improved and optimized.
       <br /><br />
-      SEO and web performance were huge gaps, and we were able to go from extremely poor scores to very good ones, raising significantly the time spent in our pages and organic accesses; consequently we raised leads conversion on visits schedule, diminishing the cost of obtaining leads and generating profit.<br /><br />
+      _ Raising page views to visits schedules conversions (core business goal) over 100%;<br />
+      _ Performance Optimization: from Very Poor scores to scores of Excelent in Desktop and Very Good on Mobile;<br />
+      _ SEO Optimization: significant growth in organic leads;<br />
+      _ Evolution of legacy code: high improvements on DevExperience and delivery/deploy speed.
+      <br /><br />
       <span>[ Stack: NextJS SSR / React & Typescript ]</span>
     </>
   );
